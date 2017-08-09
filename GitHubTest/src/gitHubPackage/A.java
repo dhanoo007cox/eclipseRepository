9e006684600621dc885e689abfd280d6f261b8ed
dhanoo007cox <30822184+dhanoo007cox@users.noreply.github.com>
2017-08-09 13:32:12 +0530

    summed up values when displaying.

--- GitHubTest/src/gitHubPackage/A.java
+++ GitHubTest/src/gitHubPackage/A.java
@@ -1,5 +1,7 @@
 package gitHubPackage;
-
+/**
+ * Represent the traditional for loop and addition
+ */
 public class A {
 	int data[] = {3, 5, 7};
 	public void display(){
@@ -8,8 +10,11 @@ public class A {
 	
 	public void m1(){
 		System.out.println("This is method m1. And array contains:");
+		int sum = 0;
 		for (int i = 0; i < data.length; i++) {
 			System.out.println(data[i]);
+			sum += i;
 		}
+		System.out.println("Sum is " + sum);
 	}
 }
