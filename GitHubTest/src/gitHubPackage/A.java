db57bca8ef752ee7f7472c8467ac9fc866ab84ab
dhanoo007cox <30822184+dhanoo007cox@users.noreply.github.com>
2017-08-09 13:22:41 +0530

    Changed default values and refactor for loop.

--- GitHubTest/src/gitHubPackage/A.java
+++ GitHubTest/src/gitHubPackage/A.java
@@ -1,15 +1,17 @@
 package gitHubPackage;
 
 public class A {
-	int data[] = {3, 5, 7};
+	int data[] = {-1, 3, 5, 7, 45};
 	public void display(){
-		System.out.println("I'm in class A");
+		System.out.println("I'm in class A\n");
 	}
 	
 	public void m1(){
 		System.out.println("This is method m1. And array contains:");
-		for (int i = 0; i < data.length; i++) {
-			System.out.println(data[i]);
+		for (int a : data) {
+			
+			System.out.print( a + " ");
 		}
+		System.out.println("\n");
 	}
 }
